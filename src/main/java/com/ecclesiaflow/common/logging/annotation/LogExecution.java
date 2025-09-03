3cc7da488648318adbed9cbca0319efad6e1b43f
GYOM15 <millimounou.guy_olivier_yanouba@courrier.uqam.ca>
2025-09-03 14:12:50 -0400

    Move annotation/ & aspect/ in common/logging

--- src/main/java/com/ecclesiaflow/common/logging/annotation/LogExecution.java
+++ src/main/java/com/ecclesiaflow/common/logging/annotation/LogExecution.java
@@ -1,4 +1,4 @@
-package com.ecclesiaflow.business.annotation;
+package com.ecclesiaflow.common.logging.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
